c4009600a308db1d07c8fca1d1c8785c7edadc21
seungjuh <wade3han@snu.ac.kr>
2023-05-19 22:35:42 -0700

    add normalize

--- generate_summary.py
+++ generate_summary.py
@@ -63,8 +63,15 @@ for name, module in model.named_modules():
 # load prompt from booksum test set
 dataset = load_dataset('kmfoda/booksum')
 dataset_test = dataset['test']
-prompt = dataset_test['chapter'][0] + ' \n What is the summary of the given text? \n '
 
+def normalize(text):
+    text = text.replace("\n", " ")
+    text = text.replace("\t", " ")
+    text = ' '.join(text.split())
+    return text
+
+prompt = normalize(dataset_test['chapter'][0]) + ' \n What is the summary of the given text? \n '
+print(prompt)
 input_ids = torch.tensor(tokenizer.encode(prompt)).unsqueeze(0).to(device=device)
 
 max_length = input_ids.shape[1] + args.genlen
